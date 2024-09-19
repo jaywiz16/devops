4c5bdce8ee23c8e2aa517176869662a9ad57e482
jaiansh sharma <113538419+jaywiz16@users.noreply.github.com>
2024-09-19 14:28:17 +0530

    Add files via upload

---
+++ second.sh
@@ -0,0 +1,16 @@
+Script started on 2024-09-17 17:02:59+05:30 [TERM="xterm-256color" TTY="/dev/pts/0" COLUMNS="151" LINES="17"]
+[?2004h]0;jaiwiz@jaiansh: /mnt/f/programming c++[01;32mjaiwiz@jaiansh[00m:[01;34m/mnt/f/programming c++[00m$ echo jai 
+[?2004l
+jai
+[?2004h]0;jaiwiz@jaiansh: /mnt/f/programming c++[01;32mjaiwiz@jaiansh[00m:[01;34m/mnt/f/programming c++[00m$ ls
+[?2004l
+ [0m[34;42mdevops[0m   [34;42mhtml[0m  [34;42m'rest code'[0m  [34;42m'sahi sai'[0m   [01;32msecond[0m   [01;32mshuruwat.cpp[0m   [01;32mshuruwat.exe[0m
+[?2004h]0;jaiwiz@jaiansh: /mnt/f/programming c++[01;32mjaiwiz@jaiansh[00m:[01;34m/mnt/f/programming c++[00m$ echo k[Kanagha
+[?2004l
+anagha
+[?2004h]0;jaiwiz@jaiansh: /mnt/f/programming c++[01;32mjaiwiz@jaiansh[00m:[01;34m/mnt/f/programming c++[00m$ exit
+[?2004l
+exit
+
+Script done on 2024-09-17 17:03:44+05:30 [COMMAND_EXIT_CODE="0"]
+   

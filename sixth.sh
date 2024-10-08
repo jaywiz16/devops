0b82d9049b881fed67c0bc89136b2ad5d5341ef3
jaiansh sharma <113538419+jaywiz16@users.noreply.github.com>
2024-10-08 14:53:31 +0530

    Add files via upload

---
+++ sixth.sh
@@ -0,0 +1,11 @@
+Script started on 2024-10-08 14:51:19+05:30 [TERM="xterm-256color" TTY="/dev/pts/2" COLUMNS="155" LINES="19"]
+[?2004h]0;jaiwiz@jaiansh: /mnt/f/programming c++[01;32mjaiwiz@jaiansh[00m:[01;34m/mnt/f/programming c++[00m$ echo hi
+[?2004lhi
+[?2004h]0;jaiwiz@jaiansh: /mnt/f/programming c++[01;32mjaiwiz@jaiansh[00m:[01;34m/mnt/f/programming c++[00m$ echo bye
+[?2004lbye
+[?2004h]0;jaiwiz@jaiansh: /mnt/f/programming c++[01;32mjaiwiz@jaiansh[00m:[01;34m/mnt/f/programming c++[00m$ echo cutu 
+[?2004lcutu
+[?2004h]0;jaiwiz@jaiansh: /mnt/f/programming c++[01;32mjaiwiz@jaiansh[00m:[01;34m/mnt/f/programming c++[00m$ exit
+[?2004lexit
+
+Script done on 2024-10-08 14:51:32+05:30 [COMMAND_EXIT_CODE="0"]
